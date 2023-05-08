44bb6716b5e0d5fc304a00b1c6b6d789495506f2
Elizabeth Salako <waleade602@gmail.com>
2023-05-08 08:17:59 -0500

    entry file

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { movieRouter } from './src/routes/movieRoute.js';
+
+dotenv.config();
+
+const app = express();
+const port = Number(process.env.PORT) || 8000
+
+app.use(express.json());
+app.use("/api/v1", movieRouter);
+
+mongoose.connect(process.env.DATABASE).then(console.log("Database connection successful")).catch((err) => {
+  console.log(err.message)
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
