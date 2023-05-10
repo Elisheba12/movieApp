9203d7af5bd48c6674de32117c5af398f93f65cb
Elizabeth Salako <waleade602@gmail.com>
2023-05-10 17:14:02 -0500

    mounted authmiddleware on required routes

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { getAllUsers, createUserReg, getUser, userUpdate, deleteUser, createUserLogin } from '../controllers/userController.js';
+import { userAuth } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 // get all movies
-router.get("/users", getAllUsers);
-router.get("/user", getUser)
+router.get("/users", userAuth, getAllUsers);
+router.get("/user", userAuth, getUser)
 router.post("/create-user-register", createUserReg);
 router.post("/create-user-login", createUserLogin);
-router.patch("/update-user", userUpdate)
-router.delete("/delete-user", deleteUser)
+router.patch("/update-user", userAuth, userUpdate)
+router.delete("/delete-user", userAuth, deleteUser)
 
 export { router as userRouter };
