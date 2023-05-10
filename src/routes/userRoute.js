74ed812471668eee82999bd48c9239ce014ec2dd
Elizabeth Salako <waleade602@gmail.com>
2023-05-10 13:20:52 -0500

    routes for user registeration and login

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
-import { getAllUsers, createUser, getUser, userUpdate, deleteUser } from '../controllers/userController.js';
+import { getAllUsers, createUserReg, getUser, userUpdate, deleteUser, createUserLogin } from '../controllers/userController.js';
 
 const router = express.Router();
 
 // get all movies
 router.get("/users", getAllUsers);
 router.get("/user", getUser)
-router.post("/create-user", createUser);
+router.post("/create-user-register", createUserReg);
+router.post("/create-user-login", createUserLogin);
 router.patch("/update-user", userUpdate)
 router.delete("/delete-user", deleteUser)
 
