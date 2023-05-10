7c365fc9327147524bcc03428946962197addcc7
Elizabeth Salako <waleade602@gmail.com>
2023-05-10 17:13:16 -0500

    mounted authmiddleware on routes

--- src/routes/movieRoute.js
+++ src/routes/movieRoute.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { getAllMovies, createMovie, getMovie, updateOneMovie, deleteMovie } from '../controllers/movieController.js';
+import { userAuth } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 // get all movies
-router.get("/movies", getAllMovies);
-router.get("/movie", getMovie)
-router.post("/create-movie", createMovie);
-router.patch("/update-movie", updateOneMovie)
-router.delete("/delete-movie", deleteMovie)
+router.get("/movies", userAuth, getAllMovies);
+router.get("/movie", userAuth, getMovie)
+router.post("/create-movie", userAuth, createMovie);
+router.patch("/update-movie", userAuth, updateOneMovie)
+router.delete("/delete-movie", userAuth, deleteMovie)
 
 export { router as movieRouter };
